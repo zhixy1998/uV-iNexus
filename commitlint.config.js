a1f981259fe7fc537c89fa5c71e6781dfa7a171b
zhixy1998 <nogiveup263@163.com>
2025-06-02 16:31:35 +0800

    feat: commit内容可以填写中文

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+   rules: {
+    'type-empty': [2, 'never'],  // 禁止 type 为空
+    'subject-empty': [2, 'never'], // 禁止 subject 为空
+    'subject-case': [0], // 关闭 subject 大小写检查（允许中文 subject）
+  }
 }
