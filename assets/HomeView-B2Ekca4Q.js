0de9abd0391c6c29c6c6dc28947ed57635029f16
zhixy1998 <94331688+zhixy1998@users.noreply.github.com>
2025-06-02 09:16:00 +0000

    Deploying to gh-pages from @ zhixy1998/uV-iNexus@71cf414bd38f1be7c547eaff1474e7c079a378be 🚀

---
+++ assets/HomeView-B2Ekca4Q.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as s,o as t,a}from"./index-Ng2IEHrJ.js";const c={},r={class:"home"};function n(_,e){return t(),s("div",r,e[0]||(e[0]=[a("h1",null,"This is an home page123",-1)]))}const m=o(c,[["render",n],["__scopeId","data-v-4ce4934d"]]);export{m as default};
